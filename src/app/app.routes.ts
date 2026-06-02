a41d3fcc2f36482937b8949a022f6a2c9a4b12d4
Akvis24 <akvis2022@gmail.com>
2026-06-02 10:19:50 +0200

    Hero Section start

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MainPage } from './shared/pages/main-page/main-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: MainPage
+    },
+    {
+        path: '**', redirectTo: '', pathMatch: 'full'
+    }
+];
---
+++ src/app/shared/pages/main-page/main-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Hero } from '../../components/hero/hero';
+
+@Component({
+  selector: 'app-main-page',
+  imports: [ Hero, ],
+  templateUrl: './main-page.html',
+  styleUrl: './main-page.scss',
+})
+export class MainPage {}
