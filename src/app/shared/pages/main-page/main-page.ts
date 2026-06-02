cdf95961bc09892372ac5646cc57fe7f1a26ef6e
Akvis24 <akvis2022@gmail.com>
2026-06-02 11:43:02 +0200

    fixes in Hero and cont.. in Why Me

--- src/app/shared/pages/main-page/main-page.ts
+++ src/app/shared/pages/main-page/main-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Hero } from '../../components/hero/hero';
+import { WhyMe } from "../../components/why-me/why-me";
 
 @Component({
   selector: 'app-main-page',
-  imports: [ Hero, ],
+  imports: [Hero, WhyMe],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss',
 })
