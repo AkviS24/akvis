ea2d5fdf09652df30659a1dc297b6e7f185abc86
Akvis24 <akvis2022@gmail.com>
2026-06-03 02:48:36 +0200

    Skill Section

--- src/app/shared/pages/main-page/main-page.ts
+++ src/app/shared/pages/main-page/main-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from '../../components/hero/hero';
 import { WhyMe } from "../../components/why-me/why-me";
+import { Skills } from '../../components/skills/skills';
 
 @Component({
   selector: 'app-main-page',
-  imports: [Hero, WhyMe],
+  imports: [Hero, WhyMe, Skills],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss',
 })
