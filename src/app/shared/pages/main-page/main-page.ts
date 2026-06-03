7b9250a133c5233e85d88505066f4539ee54f31a
Akvis24 <akvis2022@gmail.com>
2026-06-03 04:15:53 +0200

    Basis Strultur Projects Komponente

--- src/app/shared/pages/main-page/main-page.ts
+++ src/app/shared/pages/main-page/main-page.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Hero } from '../../components/hero/hero';
 import { WhyMe } from "../../components/why-me/why-me";
 import { Skills } from '../../components/skills/skills';
+import { Projects }from '../../components/projects/projects';
 
 @Component({
   selector: 'app-main-page',
-  imports: [Hero, WhyMe, Skills],
+  imports: [Hero, WhyMe, Skills, Projects],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss',
 })
