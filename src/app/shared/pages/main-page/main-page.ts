d52af77f7e63b2306f98df19535b72b19deffe0f
Akvis24 <akvis2022@gmail.com>
2026-06-08 16:05:15 +0200

    Projects go and References finished and start the contacts section

--- src/app/shared/pages/main-page/main-page.ts
+++ src/app/shared/pages/main-page/main-page.ts
@@ -3,10 +3,12 @@ import { Hero } from '../../components/hero/hero';
 import { WhyMe } from "../../components/why-me/why-me";
 import { Skills } from '../../components/skills/skills';
 import { Projects }from '../../components/projects/projects';
+import { References } from "../../components/references/references";
+import { Contact } from "../../components/contact/contact";
 
 @Component({
   selector: 'app-main-page',
-  imports: [Hero, WhyMe, Skills, Projects],
+  imports: [Hero, WhyMe, Skills, Projects, References, Contact],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss',
 })
